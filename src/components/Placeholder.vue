783961460d4d48401ad73d4566d7af17c8cf0f84
Rachel Frantsen <rachelfrantsen@gmail.com>
2017-07-08 12:31:57 -0500

    Parity with current site

---
+++ src/components/Placeholder.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="placeholder">
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'placeholder',
+    data() {
+        return {
+            message: 'Hi! This is my personal site and it\'s brand new! I plan to have it up and running soon.',
+        };
+    },
+};
+</script>
