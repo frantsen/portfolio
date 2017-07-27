f5236467530344eb9f3c50f830e9bd4a4349db01
Rachel Frantsen <rachelfrantsen@gmail.com>
2017-07-27 16:46:50 -0500

    Add Google Analytics baseline functionality

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import GoogleAnalytics from './scripts/google_analytics';
 
 Vue.config.productionTip = false;
+
+router.afterEach((to, from) => {
+  ga('set', 'page', to.fullPath);
+  ga('send', 'pageview');
+});
 
 /* eslint-disable no-new */
 new Vue({
