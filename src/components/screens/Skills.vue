3248d49cdd89adc71766663a4461720a6805000f
Rachel Frantsen <rachelfrantsen@gmail.com>
2018-05-12 13:00:24 -0500

    Clean up Skills section code

--- src/components/screens/Skills.vue
+++ src/components/screens/Skills.vue
@@ -2,15 +2,15 @@
 	<div class="skills">
 		<div class="container">
 			<div class="commit-list">
-				<span v-for="(item, index) in commits.slice().reverse()" :key="`commit-hash-${index}`">
+				<span v-for="(commit, index) in commitsDescending" :key="`commit-hash-${index}`">
 					<div class="commit-wrapper">
-						<div @click="selected=commits.length-1-index" class="hash-link">{{item.hash}}</div>
-						{{timeSince(item.date)}}
+						<div @click="selectCommit(index)" class="hash-link">{{commit.hashLabel}}</div>
+						{{commit.date | timeSinceDate}}
 					</div>
 				</span>
 			</div>
 			<div class="diff-display">
-				<div v-for="(line, index) in completeDiff" :key="`src-line-${index}`">
+				<div v-for="(line, index) in commitSrc" :key="`src-line-${index}`">
 					<div :class="['diff-line', line.type]">
 						{{line.content}}
 						<div class="message">{{line.message}}</div>
@@ -22,112 +22,125 @@
 </template>
 
 <script>
+const LineDiff = {
+	Addition: 'addition',
+	Deletion: 'deletion',
+	NoChange: 'nochange',
+};
+
+class Commit {
+	constructor(hashLabel, message, date, changes) {
+		this.hashLabel = hashLabel;
+		this.message = message;
+		this.date = date;
+		this.changes = changes;
+	}
+}
+
+class LineChange {
+	constructor(linePosition) {
+		this.position = linePosition;
+	}
+}
+
+class Addition extends LineChange {
+	constructor(linePosition, content) {
+		super(linePosition);
+		this.content = content;
+		this.type = LineDiff.Addition;
+	}
+}
+
+class Deletion extends LineChange {
+	constructor(linePosition) {
+		super(linePosition);
+		this.type = LineDiff.Deletion;
+		this.del = true;
+	}
+}
+
 export default {
 	name: 'skills',
 	data: () => ({
-		selected: 8,
+		idxSelectedCommit: 8,
 		commits: [
-			{
-				hash: 'e1b6410',
-				diff: [{position: 0, content: 'HTML: Personal site 1.0'}],
-				message: 'Inspired by my dad\'s site & others',
-				date: new Date(2002, 0),
-			},
-			{
-				hash: 'c0061d0',
-				diff: [{position: 1, content: 'Python'}],
-				message: 'Recursion clicks; coding is awesome',
-				date: new Date(2015, 1),
-			},
-			{
-				hash: 'f523646',
-				diff: [{position: 0, del: true},
-					{position: 1, content: 'JS'},
-					{position: 0, content: 'HTML'}],
-				message: 'First tech gig!',
-				date: new Date(2015, 5),
-			},
-			{
-				hash: '85aaa3f',
-				diff: [{position: 0, content: 'C++'},
-					{position: 3, del: true},
-					{position: 3, content: 'Python'}],
-				message: 'Software design; TA for Intro',
-				date: new Date(2015, 8),
-			},
-			{
-				hash: 'g3002a0',
-				diff: [{position: 4, content: 'SQL'},
-					{position: 4, content: 'Scheme'},
-					{position: 0, content: 'Android'},
-					{position: 1, del: true},
-					{position: 1, content: 'C++ TA'}],
-				message: 'Recursive AND minimalistic?? Sweet!',
-				date: new Date(2016, 1),
-			},
-			{
-				hash: 'eb9f3c5',
-				diff: [{position: 6, del: true},
-					{position: 4, content: 'Microsoft SQL Server'},
-					{position: 2, del: true},
-					{position: 2, content: 'HTML'},
-					{position: 1, content: 'ASP.NET MVC'}],
-				message: 'Intern at Proto Labs Inc.',
-				date: new Date(2016, 5),
-			},
-			{
-				hash: '46c390e',
-				diff: [{position: 2, content: 'Bash'}],
-				message: 'Adventures in scripting',
-				date: new Date(2016, 8),
-			},
-			{
-				hash: '7530344',
-				diff: [{position: 3, del: true},
-					{position: 3, content: 'C++ level up'}],
-				message: 'ALGORITHMS',
-				date: new Date(2017, 1),
-			},
-			{
-				hash: '0f830e9',
-				diff: [{position: 9, content: 'Vue.js'},
-					{position: 7, content: 'PowerShell'},
-					{position: 7, content: '.NET Core'},
-					{position: 5, content: 'Jest'},
-					{position: 4, content: 'Entity Framework Core'},
-					{position: 7, del: true}],
-				message: 'Proto Labs ECommerce project',
-				date: new Date(2017, 5),
-			},
+			new Commit('e1b6410', 'Inspired by my dad\'s site & others', new Date(2002, 0), [
+				new Addition(0, 'HTML: Personal site 1.0'),
+			]),
+			new Commit('c0061d0', 'Recursion clicks; coding is awesome', new Date(2015, 1), [
+				new Addition(1, 'Python'),
+			]),
+			new Commit('f523646', 'First tech gig!', new Date(2015, 5), [
+				new Deletion(0),
+				new Addition(1, 'JS'),
+				new Addition(0, 'HTML'),
+			]),
+			new Commit('85aaa3f', 'Software design; TA for Intro', new Date(2015, 8), [
+				new Addition(0, 'C++'),
+				new Deletion(3),
+				new Addition(3, 'Python'),
+			]),
+			new Commit('g3002a0', 'Recursive AND minimalistic?? Sweet!', new Date(2016, 1), [
+				new Addition(4, 'SQL'),
+				new Addition(4, 'Scheme'),
+				new Addition(0, 'Android'),
+				new Deletion(1),
+				new Addition(1, 'C++ TA'),
+			]),
+			new Commit('eb9f3c5', 'Intern at Protolabs Inc.', new Date(2016, 5), [
+				new Deletion(6),
+				new Addition(4, 'Microsoft SQL Server'),
+				new Deletion(2),
+				new Addition(2, 'HTML'),
+				new Addition(1, 'ASP.NET MVC'),
+			]),
+			new Commit('46c390e', 'Adventures in scripting', new Date(2016, 8), [
+				new Addition(2, 'Bash'),
+			]),
+			new Commit('7530344', 'C++ level up with ALGORITHMS', new Date(2017, 1), [
+				new Deletion(3),
+				new Addition(3, 'C++'),
+			]),
+			new Commit('0f830e9', 'Protolabs ECommerce project', new Date(2017, 5), [
+				new Addition(9, 'Vue.js'),
+				new Addition(7, 'PowerShell'),
+				new Addition(7, '.NET Core'),
+				new Addition(5, 'Jest'),
+				new Addition(4, 'Entity Framework Core'),
+				new Deletion(7),
+			]),
 		],
 	}),
 	computed: {
-		completeDiff() {
-			let lines = [];
-			for (let i = 0; i < this.selected + 1; ++i) {
-				let c = this.commits[i];
-				if (lines.length > 0) {
-					lines = lines.filter(l => l.type !== 'deleted');
+		commitsDescending() {
+			return this.commits.slice().reverse();
+		},
+		commitSrc() {
+			let linesResult = [];
+			for (let i = 0; i < this.idxSelectedCommit + 1; ++i) {
+				let commit = this.commits[i];
+				if (linesResult.length > 0) {
+					linesResult = linesResult.filter(l => l.type !== LineDiff.Deletion);
 				}
-				for (let j = 0; j < c.diff.length; ++j) {
-					let l = c.diff[j];
-					if (l.del) {
-						lines[l.position].type = 'deleted';
+				for (let j = 0; j < commit.changes.length; ++j) {
+					let line = commit.changes[j];
+					if (line.del) {
+						linesResult[line.position].type = LineDiff.Deletion;
 					} else {
-						let pos = l.position + ((lines[l.position] || {}).type === 'deleted' ? 1 : 0);
-						lines.splice(pos, 0, ({
-							type: ((i === this.selected) ? 'added' : 'unchanged'),
-							content: l.content,
-							message: c.message,
+						let pos = line.position + ((linesResult[line.position] || {}).type === LineDiff.Deletion ? 1 : 0);
+						linesResult.splice(pos, 0, ({
+							type: ((i === this.idxSelectedCommit) ? LineDiff.Addition : LineDiff.NoChange),
+							content: line.content,
+							message: commit.message,
 						}));
 					}
 				}
 			}
-			return lines;
+			return linesResult;
 		},
 	},
-	methods: {
-		timeSince(date) {
+	filters: {
+		timeSinceDate(date) {
 			let seconds = Math.floor((new Date() - date) / 1000);
 			let intervals = [{sec: 31536000, type: 'year'},
 				{sec: 2592000, type: 'month'},
@@ -138,6 +151,11 @@ export default {
 			if (!interval) return 'a moment ago';
 			let count = Math.floor(seconds / interval.sec);
 			return `${count} ${interval.type}${count > 1 ? 's' : ''} ago`;
+		},
+	},
+	methods: {
+		selectCommit(descIdxSelected) {
+			this.idxSelectedCommit = this.commits.length - 1 - descIdxSelected;
 		},
 	},
 };
@@ -187,13 +205,13 @@ export default {
 	width: 570px;
 }
 
-.unchanged {
+.nochange {
 	.message {
 		color: gray;
 	}
 }
 
-.added {
+.addition {
 	color: forestgreen;
 	background-color: #e6ffed;
 }
@@ -202,7 +220,7 @@ export default {
 	float: right;
 }
 
-.deleted {
+.deletion {
 	color: #f55;
 }
 
