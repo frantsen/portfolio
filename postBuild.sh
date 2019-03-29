67a946a3f29558b2af60b0256a260c28ff5cba34
Rachel Frantsen <rachelfrantsen@gmail.com>
2019-03-29 00:59:14 -0500

    Fix dynamic class syntax; document post-build operations

--- postBuild.sh
+++ postBuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Avoid removing folders without replacements ready
 if [ ! -e ./dist ]; then
 	echo "New build not found"
 	exit 0
@@ -11,8 +12,10 @@ TARGET_INDEX_PATH="./docs/index.html"
 rm -rf $TARGET_STATIC_DIR $TARGET_INDEX_PATH
 mv ./dist/static $TARGET_STATIC_DIR
 
+# Fix index.html paths
 CONTENT=$(<./dist/index.html)
 REPLACEMENT="src=static"
 echo "${CONTENT//src=\/static/$REPLACEMENT}" > $TARGET_INDEX_PATH
 
+# Clean up
 rm -rf ./dist
